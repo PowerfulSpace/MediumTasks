84342cc2acb2e872b12fbb63db2406efa7bf616b
Alexander <sany199574@gmail.com>
2023-08-10 00:55:29 +0300

    Проверка тестового примера

--- MediumTasks/Test/Program.cs
+++ MediumTasks/Test/Program.cs
@@ -1,22 +1,23 @@
 ﻿
-
 using System.Text.RegularExpressions;
 
-string shablon_email = @"^(?("")(""[^""]+?""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
-                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9]{2,17}))$";
-while (true)
+
+
+string input = "Yes. This dog is very friendly.";
+string pattern = @"((\w+)[\s.])+";
+foreach (Match match in Regex.Matches(input, pattern))
 {
-    Console.WriteLine("Укажите email");
-    string email = Console.ReadLine();
-    if (Regex.IsMatch(email, shablon_email, RegexOptions.IgnoreCase))
+    Console.WriteLine("Match: {0}", match.Value);
+    for (int groupCtr = 0; groupCtr < match.Groups.Count; groupCtr++)
     {
-        Console.WriteLine("Адрес электронной почты" + email + " введен верно");
-        break;
-    }
-    else
-    {
-        Console.WriteLine("Введенный адрес электронной почты " + email + " некорректен");
+        Group group = match.Groups[groupCtr];
+        Console.WriteLine("   Group {0}: {1}", groupCtr, group.Value);
+        for (int captureCtr = 0; captureCtr < group.Captures.Count; captureCtr++)
+            Console.WriteLine("      Capture {0}: {1}", captureCtr,
+                              group.Captures[captureCtr].Value);
     }
 }
 
 Console.ReadLine();
+
+
