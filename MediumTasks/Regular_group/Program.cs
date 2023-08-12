098323e595243101270d3a31fdb270b98f07eddd
Alexander <sany199574@gmail.com>
2023-08-12 20:57:03 +0300

    Добавлин пример, работа с группой

--- MediumTasks/Regular_group/Program.cs
+++ MediumTasks/Regular_group/Program.cs
@@ -9,7 +9,7 @@ MatchCollection matches = Regex.Matches(input, pattern);
 
 foreach (Match match in matches)
 {
-    Console.WriteLine("Full telephone number: {0}",match);
+    Console.WriteLine("Full telephone number: {0}", match);
     Console.WriteLine("Area Code:             {0}", match.Groups[1].Value);
     Console.WriteLine("Telephone number:      {0}", match.Groups[2].Value);
     Console.WriteLine();
