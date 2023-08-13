8693f216eb1e8644d4aff217821f12fb50721c27
Alexander <sany199574@gmail.com>
2023-08-14 00:39:29 +0300

    Добавлено приложение, которое удаляет знак валюты в числах

---
+++ MediumTasks/Regular_FindingUppercaseWords/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using System.Text.RegularExpressions;
+
+string pattern = @"\b[A-Z]\w*\b";
+string input = "A city Albany Zulu maritime Marseilles";
+foreach (Match match in Regex.Matches(input, pattern))
+    Console.WriteLine(match.Value);
+
+Console.ReadLine();
+
---
+++ MediumTasks/Regular_negation/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Text.RegularExpressions;
+
+
+
+string pattern = @"\bth[^o]\w+\b";
+string input = "thought thing though them through thus thorough this";
+foreach (Match match in Regex.Matches(input, pattern))
+    Console.WriteLine(match.Value);
+
+
+
+Console.ReadLine();
