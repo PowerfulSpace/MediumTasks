b6efe81ff0aa2bc223eb761810d7e29c3124d86a
Alexander <sany199574@gmail.com>
2023-08-15 01:29:25 +0300

    Исправил, и полностью доделал прилодение, которое ищет повторяющие слова и заменяет их на одно слово внутри тега string

---
+++ MediumTasks/Regular_ValidNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Text.RegularExpressions;
+
+
+string pattern = @"^(\(?\d{3}\)?[\s-])?\d{3}-\d{4}$";
+string[] inputs = { "111 111-1111", "222-2222", "222 333-444",
+                          "(212) 111-1111", "111-AB1-1111",
+                          "212-111-1111", "01 999-9999" };
+
+foreach (string input in inputs)
+{
+    var result = Regex.IsMatch(input, pattern);
+    IsValidNumber(result, input);
+
+}
+Console.ReadLine();
+
+static void IsValidNumber(bool isMatch, string input)
+{
+    if (isMatch)
+    {
+        Console.Write("{0,-15}", input);
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("{0}", ": matched");
+    }
+    else
+    {
+        Console.Write("{0,-15}", input);
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("{0}", ": match failed");
+
+    }
+    Console.ForegroundColor = ConsoleColor.White;
+}
---
+++ MediumTasks/Regular_lazyLodaing/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Text.RegularExpressions;
+
+string regexp = @""".+?"" ";
+
+
+string str = "a \"witch\" and her \"broom\" is one";
+
+Regex regex = new Regex(regexp);
+
+var result = regex.Matches(str);
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
+
+Console.ReadLine();
