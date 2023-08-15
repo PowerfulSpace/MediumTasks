884ef74804df1271ea525618fdb841749f588830
Alexander <sany199574@gmail.com>
2023-08-16 00:05:09 +0300

    Разобр примера, на  работу с результатом только последней входнйо строки

--- MediumTasks/Test/Program.cs
+++ MediumTasks/Test/Program.cs
@@ -1,40 +1,22 @@
 ﻿
 using System.Text.RegularExpressions;
 
-string input = "Brooklyn Dodgers, National League, 1911, 1912, 1932-1957\n" +
-                     "Chicago Cubs, National League, 1903-present\n" +
-                     "Detroit Tigers, American League, 1901-present\n" +
-                     "New York Giants, National League, 1885-1957\n" +
-                     "Washington Senators, American League, 1901-1960\n";
-string pattern = @"^((\w+(\s?)){2,}),\s(\w+\s\w+),(\s\d{4}(-(\d{4}|present))?,?)+";
-Match match;
+string[] inputs = { "Brooklyn Dodgers, National League, 1911, 1912, 1932-1957",
+                          "Chicago Cubs, National League, 1903-present" + Environment.NewLine,
+                          "Detroit Tigers, American League, 1901-present" + Regex.Unescape(@"\n"),
+                          "New York Giants, National League, 1885-1957",
+                          "Washington Senators, American League, 1901-1960" + Environment.NewLine};
+string pattern = @"^((\w+(\s?)){2,}),\s(\w+\s\w+),(\s\d{4}(-(\d{4}|present))?,?)+\r?\Z";
 
-match = Regex.Match(input, pattern);
-while (match.Success)
+foreach (string input in inputs)
 {
-    Console.Write("The {0} played in the {1} in",
-                      match.Groups[1].Value, match.Groups[4].Value);
-    foreach (Capture capture in match.Groups[5].Captures)
-        Console.Write(capture.Value);
-
-    Console.WriteLine(".");
-    match = match.NextMatch();
+    Console.WriteLine(Regex.Escape(input));
+    Match match = Regex.Match(input, pattern);
+    if (match.Success)
+        Console.WriteLine("   Match succeeded.");
+    else
+        Console.WriteLine("   Match failed.");
 }
-
-Console.WriteLine();
-
-match = Regex.Match(input, pattern, RegexOptions.Multiline);
-while (match.Success)
-{
-    Console.Write("The {0} played in the {1} in",
-                      match.Groups[1].Value, match.Groups[4].Value);
-    foreach (Capture capture in match.Groups[5].Captures)
-        Console.Write(capture.Value);
-
-    Console.WriteLine(".");
-    match = match.NextMatch();
-}
-Console.WriteLine();
 
 Console.ReadLine();
 
