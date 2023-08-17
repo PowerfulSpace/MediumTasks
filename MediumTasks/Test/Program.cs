05364723d428293723879d887ee4ce2ca0e7200b
Alexander <sany199574@gmail.com>
2023-08-17 06:05:14 +0300

    Добавлен пример поиск предложений которые включат в себя не более 10 слов

--- MediumTasks/Test/Program.cs
+++ MediumTasks/Test/Program.cs
@@ -1,8 +1,11 @@
 ﻿
 using System.Text.RegularExpressions;
 
-string pattern = @"\b(\w{3,}?\.){2}?\w{3,}?\b";
-string input = "www.microsoft.com msdn.microsoft.com mywebsite mycompany.com";
+string pattern = @"\b[A-Z](\w*?\s*?){1,10}[.!?]";
+string input = "Hi. I am writing a short note. Its purpose is " +
+                      "to test a regular expression that attempts to find " +
+                      "sentences with ten or fewer words. Most sentences " +
+                      "in this note are short.";
 foreach (Match match in Regex.Matches(input, pattern))
     Console.WriteLine("'{0}' found at position {1}.", match.Value, match.Index);
 
