b3f51b7708eb8a28bc6f3847e81c85a6200c404a
Alexander <sany199574@gmail.com>
2023-08-17 05:20:33 +0300

    Добавлен пример, который показывает как работает ленивое совпадение

--- MediumTasks/Regular_lazyLodaing/Program.cs
+++ MediumTasks/Regular_lazyLodaing/Program.cs
@@ -1,18 +1,33 @@
 ﻿
 using System.Text.RegularExpressions;
 
+//Без ленивого совпадения не корректно отрабатывает код
 string regexp = @""".+?"" ";
-
-
 string str = "a \"witch\" and her \"broom\" is one";
-
 Regex regex = new Regex(regexp);
-
 var result = regex.Matches(str);
 
 foreach (var item in result)
 {
     Console.WriteLine(item);
 }
+
+Console.WriteLine();
+
+//Демонстративно показано как рабоатет ленивое совпадение
+string pattern = @"^\s*(System.)??Console.Write(Line)??\(??";
+string input = "System.Console.WriteLine(\"Hello!\")\n" +
+                      "Console.Write(\"Hello!\")\n" +
+                      "Console.WriteLine(\"Hello!\")\n" +
+                      "Console.ReadLine()\n" +
+                      "   Console.WriteLine";
+
+Console.WriteLine();
+foreach (Match match in Regex.Matches(input, pattern,
+                                      RegexOptions.IgnorePatternWhitespace |
+                                      RegexOptions.IgnoreCase |
+                                      RegexOptions.Multiline))
+    Console.WriteLine("'{0}' found at position {1}.", match.Value, match.Index);
+
 
 Console.ReadLine();
--- MediumTasks/Test/Program.cs
+++ MediumTasks/Test/Program.cs
@@ -1,20 +1,19 @@
 ﻿
 using System.Text.RegularExpressions;
 
-string pattern = @"(\b(\w+)\W+)+";
-string input = "This is a short sentence.";
-Match match = Regex.Match(input, pattern);
-Console.WriteLine("Match: '{0}'", match.Value);
-for (int ctr = 1; ctr < match.Groups.Count; ctr++)
-{
-    Console.WriteLine("   Group {0}: '{1}'", ctr, match.Groups[ctr].Value);
-    int capCtr = 0;
-    foreach (Capture capture in match.Groups[ctr].Captures)
-    {
-        Console.WriteLine("      Capture {0}: '{1}'", capCtr, capture.Value);
-        capCtr++;
-    }
-}
+string pattern = @"^\s*(System.)??Console.Write(Line)??\(??";
+string input = "System.Console.WriteLine(\"Hello!\")\n" +
+                      "Console.Write(\"Hello!\")\n" +
+                      "Console.WriteLine(\"Hello!\")\n" +
+                      "Console.ReadLine()\n" +
+                      "   Console.WriteLine";
+
+Console.WriteLine();
+foreach (Match match in Regex.Matches(input, pattern,
+                                      RegexOptions.IgnorePatternWhitespace |
+                                      RegexOptions.IgnoreCase |
+                                      RegexOptions.Multiline))
+    Console.WriteLine("'{0}' found at position {1}.", match.Value, match.Index);
 
 
 Console.ReadLine();
