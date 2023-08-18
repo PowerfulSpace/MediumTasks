9444de66fb6419973e79237a0ba204948afca6a4
Alexander <sany199574@gmail.com>
2023-08-18 04:07:01 +0300

    Доабвлен пример, который сопоставляет выражение по группам захвата

--- MediumTasks/Regular_CaptureGropu_Base/Program.cs
+++ MediumTasks/Regular_CaptureGropu_Base/Program.cs
@@ -19,6 +19,40 @@ foreach (Match match in Regex.Matches(input2, pattern2))
                       match.Value, match.Index);
 
 
+//uгруппы захвата. a совпадает с a и дописываем b. Первая группа захвата
+string pattern3 = @"(?<1>a)(?<1>\1b)*";
+string input3 = "aababb";
+foreach (Match match in Regex.Matches(input3, pattern3))
+{
+    Console.WriteLine("Match: " + match.Value);
+    foreach (Group group in match.Groups)
+        Console.WriteLine("   Group: " + group.Value);
+}
+
+
+//сопоставление шаблона с группами захвата. \p{Lu} -  заглавные буквы
+string pattern4 = @"\b(\p{Lu}{2})(\d{2})?(\p{Lu}{2})\b";
+string[] inputs4 = { "AA22ZZ", "AABB" };
+foreach (string input4 in inputs4)
+{
+    Match match = Regex.Match(input4, pattern4);
+    if (match.Success)
+    {
+        Console.WriteLine("Match in {0}: {1}", input4, match.Value);
+        if (match.Groups.Count > 1)
+        {
+            for (int ctr = 1; ctr <= match.Groups.Count - 1; ctr++)
+            {
+                if (match.Groups[ctr].Success)
+                    Console.WriteLine("Group {0}: {1}",
+                                      ctr, match.Groups[ctr].Value);
+                else
+                    Console.WriteLine("Group {0}: <no match>", ctr);
+            }
+        }
+    }
+    Console.WriteLine();
+}
 
 
 Console.ReadLine();
--- MediumTasks/Test/Program.cs
+++ MediumTasks/Test/Program.cs
@@ -1,11 +1,29 @@
 ﻿
 using System.Text.RegularExpressions;
 
-string pattern = @"(?<char>\w)\k<char>";
-string input = "trellis llama webbing dresser swagger";
-foreach (Match match in Regex.Matches(input, pattern))
-    Console.WriteLine("Found '{0}' at position {1}.",
-                      match.Value, match.Index);
+string pattern = @"\b(\p{Lu}{2})(\d{2})?(\p{Lu}{2})\b";
+string[] inputs = { "AA22ZZ", "AABB" };
+foreach (string input in inputs)
+{
+    Match match = Regex.Match(input, pattern);
+    if (match.Success)
+    {
+        Console.WriteLine("Match in {0}: {1}", input, match.Value);
+        if (match.Groups.Count > 1)
+        {
+            for (int ctr = 1; ctr <= match.Groups.Count - 1; ctr++)
+            {
+                if (match.Groups[ctr].Success)
+                    Console.WriteLine("Group {0}: {1}",
+                                      ctr, match.Groups[ctr].Value);
+                else
+                    Console.WriteLine("Group {0}: <no match>", ctr);
+            }
+        }
+    }
+    Console.WriteLine();
+}
+
 
 Console.ReadLine();
 
