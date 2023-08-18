bbbb64ad0eee63a52f0a517c446019c6fbd9118c
Alexander <sany199574@gmail.com>
2023-08-18 03:38:06 +0300

    Добавлен пример, базовой группировки

---
+++ MediumTasks/Regular_CaptureGropu_Base/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Text.RegularExpressions;
+
+//В этом примере 2 группы, 0 группа - это весь шаблон, 1 группа, та которая вошла в скобки(). Происходит
+//поиск идентичной первой группы
+string pattern = @"(\w)\1";
+string input = "trellis llama webbing dresser swagger";
+foreach (Match match in Regex.Matches(input, pattern))
+    Console.WriteLine("Found '{0}' at position {1}.",
+                      match.Value, match.Index);
+
+
+Console.ReadLine();
+
+
--- MediumTasks/Regular_ExecudedGroup/Program.cs
+++ MediumTasks/Regular_ExecudedGroup/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 using System.Text.RegularExpressions;
 
+//Вычитаем из группы от 0-9 все чётные цифры 2468
 string[] inputs = { "123", "13579753", "3557798", "335599901" };
 string pattern = @"^[0-9-[2468]]+$";
 
