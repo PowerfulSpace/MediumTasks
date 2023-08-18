3944a660ca8c63e9d9fa758afdab3caa5cb40516
Alexander <sany199574@gmail.com>
2023-08-18 05:20:39 +0300

    Добавлен пример на удаление символов валюты, с использованием именнованной группы

--- MediumTasks/Test/Program.cs
+++ MediumTasks/Test/Program.cs
@@ -1,8 +1,8 @@
 ﻿
 using System.Text.RegularExpressions;
 
-string pattern = @"\p{Sc}*(\s?\d+[.,]?\d*)\p{Sc}*";
-string replacement = "$1";
+string pattern = @"\p{Sc}*(?<amount>\s?\d+[.,]?\d*)\p{Sc}*";
+string replacement = "${amount}";
 string input = "$16.32 12.19 £16.29 €18.29  €18,29";
 string result = Regex.Replace(input, pattern, replacement);
 Console.WriteLine(result);
