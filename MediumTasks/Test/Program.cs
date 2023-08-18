e4bfb33112c958847d373d7888d9fe23c36196f3
Alexander <sany199574@gmail.com>
2023-08-18 05:26:23 +0300

    Добавлен пример для рассмотрения. Где используется кеультура пк. Для сопоставления валюты

--- MediumTasks/Test/Program.cs
+++ MediumTasks/Test/Program.cs
@@ -1,11 +1,24 @@
 ﻿
+using System.Globalization;
 using System.Text.RegularExpressions;
 
-string pattern = @"\p{Sc}*(?<amount>\s?\d+[.,]?\d*)\p{Sc}*";
-string replacement = "${amount}";
-string input = "$16.32 12.19 £16.29 €18.29  €18,29";
-string result = Regex.Replace(input, pattern, replacement);
-Console.WriteLine(result);
+// Define array of decimal values.
+string[] values = { "16.35", "19.72", "1234", "0.99" };
+// Determine whether currency precedes (True) or follows (False) number.
+bool precedes = NumberFormatInfo.CurrentInfo.CurrencyPositivePattern % 2 == 0;
+// Get decimal separator.
+string cSeparator = NumberFormatInfo.CurrentInfo.CurrencyDecimalSeparator;
+// Get currency symbol.
+string symbol = NumberFormatInfo.CurrentInfo.CurrencySymbol;
+// If symbol is a "$", add an extra "$".
+if (symbol == "$") symbol = "$$";
+
+// Define regular expression pattern and replacement string.
+string pattern = @"\b(\d+)(" + cSeparator + @"(\d+))?";
+string replacement = "$1$2";
+replacement = precedes ? symbol + " " + replacement : replacement + " " + symbol;
+foreach (string value in values)
+    Console.WriteLine("{0} --> {1}", value, Regex.Replace(value, pattern, replacement));
 
 Console.ReadLine();
 
