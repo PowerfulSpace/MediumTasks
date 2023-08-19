959aa2a06a5fafa4c691890149a7c68291e4a4c9
Alexander <sany199574@gmail.com>
2023-08-19 05:54:03 +0300

    Добавлен тестовый пример, с ленивым квантификатором

--- MediumTasks/Test/Program.cs
+++ MediumTasks/Test/Program.cs
@@ -2,10 +2,26 @@
 
 using System.Text.RegularExpressions;
 
-var r = new Regex(@"^(?<name>\w+):(?<value>\w+)");
-Match m = r.Match("Section1:119900");
-Console.WriteLine(m.Groups["name"].Value);
-Console.WriteLine(m.Groups["value"].Value);
+string greedyPattern = @".+(\d+)\.";
+string lazyPattern = @".+?(\d+)\.";
+string input = "This sentence ends with the number 107325.";
+Match match;
+
+// Match using greedy quantifier .+.
+match = Regex.Match(input, greedyPattern);
+if (match.Success)
+    Console.WriteLine("Number at end of sentence (greedy): {0}",
+                      match.Groups[1].Value);
+else
+    Console.WriteLine("{0} finds no match.", greedyPattern);
+
+// Match using lazy quantifier .+?.
+match = Regex.Match(input, lazyPattern);
+if (match.Success)
+    Console.WriteLine("Number at end of sentence (lazy): {0}",
+                      match.Groups[1].Value);
+else
+    Console.WriteLine("{0} finds no match.", lazyPattern);
 
 Console.ReadLine();
 
