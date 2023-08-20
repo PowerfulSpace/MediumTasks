3e8697a799e3c1b952cee3efc4ff74741f3998f3
Alexander <sany199574@gmail.com>
2023-08-20 04:06:33 +0300

    Почистил тестовый пример

--- MediumTasks/Test/Program.cs
+++ MediumTasks/Test/Program.cs
@@ -1,28 +1,10 @@
-﻿
+﻿using System.Text.RegularExpressions;
 
-using System.Text.RegularExpressions;
-
-
-string a = "dsa,d";
-Example.CleanInput(a);
+string testString = "John Doe, 42 years";
+Regex regex = new Regex("[0-9]+");
+if (regex.IsMatch(testString))
+    Console.WriteLine("String contains numbers!");
+else
+    Console.WriteLine("String does NOT contain numbers!");
 
 Console.ReadLine();
-
-static public class Example
-{
-    static public string CleanInput(string strIn)
-    {
-        // Replace invalid characters with empty strings.
-        try
-        {
-            return Regex.Replace(strIn, @"[^\w\.@-]", "",
-                                 RegexOptions.None, TimeSpan.FromSeconds(1.5));
-        }
-        // If we timeout when replacing invalid characters,
-        // we should return Empty.
-        catch (RegexMatchTimeoutException)
-        {
-            return String.Empty;
-        }
-    }
-}
