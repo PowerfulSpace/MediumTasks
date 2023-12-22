6ac6b640903580e9550a25511f4096f170c93ad6
Alexander <sany199574@gmail.com>
2023-12-22 08:40:42 +0300

    Добавлена вспомогательная информация про чувствительность к регистру

--- MediumTasks/Test/Program.cs
+++ MediumTasks/Test/Program.cs
@@ -1,10 +1,20 @@
 ﻿using System.Text.RegularExpressions;
 
-string testString = "John Doe, 42 years";
-Regex regex = new Regex("[0-9]+");
-if (regex.IsMatch(testString))
-    Console.WriteLine("String contains numbers!");
-else
-    Console.WriteLine("String does NOT contain numbers!");
+string result = string.Empty;
+
+result = Regex.Replace("04/17/2012",
+                        "(?<month>\\d{1,2})/(?<day>\\d{1,2})/(?<year>\\d{2,4})",
+                        "${day}-${month}-${year}");
+
+Console.WriteLine(result);
+
+result = Regex.Replace("@_H e l l o-777.,:;'!@#$%^&*()_-+<>?/",
+                      @"[^\w\.@_-]", "");
+
+
+Console.WriteLine(result);
+
+Console.ReadKey();
+
 
 Console.ReadLine();
